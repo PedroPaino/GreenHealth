729750f68855957a0e2ea46b657addad5da96342
Pedro Paino <94409110+PedroPaino@users.noreply.github.com>
2023-10-26 14:25:21 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,23 @@ window.addEventListener('scroll', function () {
 if (this.window.pageYOffset > 0) return navbar.classList.add('active');
 return navbar.classList.remove('active');
 });
+
+// Selecione os botões
+const modoClaroButton = document.getElementById('modoClaro');
+const modoEscuroButton = document.getElementById('modoEscuro');
+
+// Função para ativar o modo escuro
+function ativarModoEscuro() {
+    // Adicione uma classe à tag HTML que define o estilo escuro
+    document.documentElement.classList.add('modo-escuro');
+}
+
+// Função para ativar o modo claro
+function ativarModoClaro() {
+    // Remova a classe que define o estilo escuro
+    document.documentElement.classList.remove('modo-escuro');
+}
+
+// Ouvintes de eventos para os botões
+modoEscuroButton.addEventListener('click', ativarModoEscuro);
+modoClaroButton.addEventListener('click', ativarModoClaro);
